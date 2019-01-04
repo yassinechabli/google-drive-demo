da13514ca4f8c2ab946be1796f13e5356ade4124
mychabli <mychabli@octo.com>
2019-01-04 17:30:34 +0100

    first commit

---
+++ src/oauth2callback.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+    session_start();
+    try{
+        $client = new Google_Client();
+        $client->setAuthConfigFile('credentials.json');
+        $client->setRedirectUri('http://' . $_SERVER['HTTP_HOST'] . '/google_drive/oauth2callback.php');
+        $client->addScope(Google_Service_Drive::DRIVE_METADATA_READONLY);
+
+        if (! isset($_GET['code'])) {
+            $auth_url = $client->createAuthUrl();
+            header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
+        } else {
+            $client->authenticate($_GET['code']);
+            $_SESSION['access_token'] = $client->getAccessToken();
+            $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/google_drive';
+            header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+    }
+
+    }catch (Exception $e){
+        var_dump($e->getMessage());
+    }
